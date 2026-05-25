0aa7d60332bb1831265ccd3f07653872802dd9e8
Satyam Dwivedi <122256218+satyamdwivedi7@users.noreply.github.com>
2026-05-25 16:43:36 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char c : s){
+            if(!st.empty() && st.top() == '(' && c == ')'){
+                st.pop();
+            }
+            else if(!st.empty() && st.top() == '{' && c == '}'){
+                st.pop();
+            }
+            else if(!st.empty() && st.top() == '[' && c == ']'){
+                st.pop();
+            }
+            else{
+                st.push(c);
+            }
+        }
+        if(st.empty()){
+            return true;
+        }
+        return false;
+    }
+};
